77588ca1b016b7a72012dda7869165f59175ea95
Kron Austrum <huyach@protonmail.ch>
2020-01-16 11:01:57 +0300

    ++++

--- keymap.c
+++ keymap.c
@@ -110,10 +110,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
         else
             {
                 unregister_mods(MOD_LSFT);
-                if (timer_elapsed(key_timer) < 200)
+                if (timer_elapsed(key_timer) < 500)
                     {
                     layer_invert(RUS);
-                    tap_code16(S(A(KC_NO)));
+                    tap_code16(C(G(KC_NO)));
                     }
                 else
                     {
@@ -180,11 +180,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRAVE ,          KC_EXCLAIM,  KC_AT,  KC_HASH, KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_QUESTION, KC_ASTERISK, AKM_1_9, KC_UNDERSCORE, KC_MINUS, KC_EQUAL,  KC_DOUBLE_QUOTE, AKM_1_14,
         KC_TAB   ,          KC_Q,        KC_W,   KC_E,    KC_R,      KC_T,       KC_Y,          KC_U,        KC_I,        KC_O,    KC_P,          AKM_2_22, AKM_2_23,  KC_BSPC,
         KC_GESC  ,          KC_A,        KC_S,   KC_D,    KC_F,      KC_G,       KC_H,          KC_J,        KC_K,        KC_L,    AKM_3_11,      KC_QUOT,  KC_ENT,
-        RUS_LAT,XXXXXXX,  KC_Z,        KC_X,   KC_C,    KC_V,      KC_B,       KC_N,          KC_M,        KC_COMM,     KC_DOT,  AKM_4_11,      KC_UP,    MO(NAV),
+        SFT_T(KC_CAPS),XXXXXXX,  KC_Z,   KC_X,   KC_C,    KC_V,      KC_B,       KC_N,          KC_M,        KC_COMM,     KC_DOT,  AKM_4_11,      KC_UP,    RUS_LAT,
         KC_LCTL  ,KC_LGUI,  KC_LALT,             MO(FN),             KC_ENT,                    KC_SPC,      MO(FN),      KC_APP,  KC_LEFT,       KC_DOWN,  KC_RIGHT),
 
     [RUS] = LAYOUT(
-        _______,          _______,      _______, _______, _______,  _______,     _______,       S(KC_7),     _______,     _______,  _______,      _______,  _______,     _______,   _______,
+        _______,          _______,      UC(0x0040), KC_AT, _______,  _______,     _______,    S(KC_7),     _______,     _______,  _______,      _______,  _______,     _______,   _______,
         _______,          KC_Q,         KC_W,    KC_E,    KC_R,     KC_T,        KC_Y,          KC_U,        KC_I,        KC_O,     KC_P,         KC_LBRC,  S(KC_SLASH), _______,
         _______,          KC_A,         KC_S,    KC_D,    KC_F,     KC_G,        KC_H,          KC_J,        KC_K,        KC_L,     KC_SCLN,      KC_QUOT,  _______,
         _______,XXXXXXX,  KC_Z,         KC_X,    KC_C,    KC_V,     KC_B,        KC_N,          M_CYR_ER,    KC_COMM,     KC_DOT,   KC_SLSH,      _______,  _______,
@@ -192,9 +192,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [FN] = LAYOUT(
         RESET  ,          KC_F1,        KC_F2,   KC_F3,   KC_F4,    KC_F5,       KC_F6,         KC_F7,       KC_F8,       KC_F9,    KC_F10,       KC_F11,         KC_F12,      KC_INS,    KC_PAUS,
-        _______,          RGB_TOG,      RGB_MOD, RGB_RMOD,RGB_HUI,  RGB_HUD,     RGB_SPI,       KC_KP_MINUS, KC_7,        KC_8,     KC_9,         KC_KP_ASTERISK, KC_KP_SLASH, KC_PSCR,
-        KC_CAPSLOCK,      RGB_SAI,      RGB_SAD, _______, KC_DEL,   KC_HOME,     KC_END,        KC_BSPC,     KC_4,        KC_5,     KC_6,         KC_KP_EQUAL,    KC_PDOT,
-        LALT(KC_LSHIFT),XXXXXXX,  RGB_M_P,      RGB_M_R, RGB_M_X, RGB_M_G,  RGB_M_SW,    RGB_SPD,       KC_KP_PLUS,  KC_1,        KC_2,     KC_3,         _______,        _______,
+        _______,          KC_HOME,      KC_UP,   KC_END,   _______,  _______,     _______,       KC_KP_MINUS, KC_7,        KC_8,     KC_9,         KC_KP_ASTERISK, KC_KP_SLASH, _______,
+        KC_CAPSLOCK,      KC_LEFT,      KC_DOWN, KC_RIGHT, KC_DEL,   KC_HOME,     KC_END,        KC_BSPC,     KC_4,        KC_5,     KC_6,         KC_KP_EQUAL,    KC_PDOT,
+        LALT(KC_LSHIFT),XXXXXXX,  KC_DOWN,      KC_PGUP, _______, _______,  _______,    _______,       KC_KP_PLUS,  KC_1,        KC_2,     KC_3,         _______,        _______,
         _______,_______,  _______,               _______,           TO(LAT),                    KC_0,        _______,     _______,  _______,      _______,        _______),
 
     [NAV] = LAYOUT(
